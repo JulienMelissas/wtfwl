6bc1b935fc2ae9cd858218b9c93c0e5098874300
Julien Melissas <julien.melissas@gmail.com>
2017-12-26 17:12:30 -0500

    Added some basic styles, text, etc.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+// React
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+// Styles
+import 'sanitize.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="wrap">
+          <header className="App-header">
+            <h1 className="App-title">What The Fuck is the Weather Like?</h1>
+          </header>
+          <div className="form">
+          Something here.
+          </div>
+        </div>
       </div>
     );
   }
