b09127e3ad94e13c1617ff4b3e579567c99a98f1
Julien Melissas <julien.melissas@gmail.com>
2017-12-28 04:45:42 -0500

    First working component for location search hooked up and working. Needs to be styled!

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
 import { Redirect } from 'react-router-dom'
 
 import Geolocation from 'react-geolocation'
+
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete'
 
 class Finder extends React.Component {
   state = {latitude: '', longitude: ''}
@@ -24,7 +28,7 @@ class Finder extends React.Component {
             : fetchingPosition
             ? <p>Being creepy...</p>
             : <p>
-              <a onClick={getCurrentPosition}>Find Me</a>
+              <a onClick={getCurrentPosition}>Find Me.</a>
               {fetchingPosition}
               {error &&
               <p>
@@ -38,12 +42,65 @@ class Finder extends React.Component {
   }
 }
 
+class SimpleForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {address: '', latLng: false}
+    this.onChange = (address) => this.setState({address})
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => this.setState({latLng: latLng}))
+      .catch(error => console.error('Error', error))
+  }
+
+  render () {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+      placeholder: 'Somewhere, USA',
+    }
+
+    if (this.state.latLng) {
+      return <Redirect to={`/${this.state.latLng.lat}/${this.state.latLng.lng}/`}/>
+    }
+
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <PlacesAutocomplete inputProps={inputProps}/>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
+
+class Search extends React.Component {
+  state = {showSearch: false}
+
+  render () {
+    let toggleShowSearch = () => {
+      this.setState({showSearch: !this.state.showSearch})
+    }
+    return (
+      this.state.showSearch
+        ? <SimpleForm/>
+        : <a onClick={toggleShowSearch}>Fuck you very much,<br/>I'll find myself.</a>
+
+    )
+  }
+}
+
 const Home = () => (
   <div className="view--home">
     <header className="app-header">
       <h1 className="app-title">What The Fuck is the Weather Like?</h1>
     </header>
     {<Finder/>}
+    {<Search/>}
   </div>
 )
 
